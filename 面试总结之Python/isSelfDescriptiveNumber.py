3652c85c23b637c3643520f9d2da1d76f6379e86
haoran119 <34557994+haoran119@users.noreply.github.com>
2021-09-05 22:49:01 +1000

    Update isSelfDescriptiveNumber.py

--- 面试总结之Python/isSelfDescriptiveNumber.py
+++ 面试总结之Python/isSelfDescriptiveNumber.py
@@ -1,7 +1,6 @@
 """
-A self-descriptive number is an integer n in given base b is b digits long in which each digit at position
-p (the most significant digit being at position 0 and the least significant at position b – 1) counts how
-many times a digit p is in n.
+A self-descriptive number is an integer n in given base b is b digits long in which each digit at position p (the most significant digit being at position 0 
+and the least significant at position b – 1) counts how many times a digit p is in n.
 
 For example in base 10, 6210001000 is a self descriptive number.
 
