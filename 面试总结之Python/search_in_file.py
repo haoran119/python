83776d18f8609d3140e63b4ea43076a3020a86a5
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-02-13 20:31:03 +1100

    Update search_in_file.py

--- 面试总结之Python/search_in_file.py
+++ 面试总结之Python/search_in_file.py
@@ -20,6 +20,20 @@ class Solution:
                     fout.write('{}\n'.format(line))
                     print(line, end='\n')
 
+    def search_in_file_w_re(self, input_file: str, output_file: str, key: str) -> None:
+        import re
+
+        pattern = re.compile('(.*{}.*)'.format(key))
+
+        with open(input_file, 'r') as fin:
+            text = fin.read()
+            results = re.findall(pattern, text)
+
+        with open(output_file, 'w') as fout:
+            for result in results:
+                fout.write('{}\n'.format(result))
+                print(result, end='\n')
+
 
 if __name__ == '__main__':
     mySolution = Solution()
@@ -28,3 +42,5 @@ if __name__ == '__main__':
     search_key = 'file'
 
     mySolution.search_in_file(input, output, search_key)
+
+    mySolution.search_in_file_w_re(input, output, search_key)
