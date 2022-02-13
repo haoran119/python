cdc000f3aa06a19c47dfb55ce287a369148ef0a3
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-02-13 22:08:59 +1100

    Delete How to iterate directory for files.py

--- 学习笔记之Python/How to iterate directory for files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-root = '.'
-
-for path, subdirs, files in os.walk(root):
-    for name in files:
-        if name.endswith('.json'):
-            filename = os.path.join(path, name)
-            print(filename) # '.\path\test.json'
