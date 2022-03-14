8dc1d3766c156fe88d2c7a6c886d095c7eba1968
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-03-14 22:17:43 +1100

    Update count_char_occurrence_in_string.py

--- 面试总结之Python/count_char_occurrence_in_string.py
+++ 面试总结之Python/count_char_occurrence_in_string.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 https://www.geeksforgeeks.org/program-count-occurrence-given-character-string/?ref=gcse
-
-Given a string and a character, task is to make a function which count
-occurrence of the given character in the string.
+Given a string and a character, task is to make a function
+which count occurrence of the given character in the string.
 """
 
 
@@ -18,9 +17,16 @@ def count_char_occurrence_in_string(s: str, c: str) -> int:
     return result
 
 
+def count_char_occurrence_in_string1(s: str, c: str) -> int:
+    return s.count(c)
+
+
 if __name__ == '__main__':
     s_input = "geeksforgeeks"
     c_input = 'e'
 
     result = count_char_occurrence_in_string(s_input, c_input)
-    print(result)
+    print(result)   # 4
+
+    result = count_char_occurrence_in_string1(s_input, c_input)
+    print(result)   # 4
