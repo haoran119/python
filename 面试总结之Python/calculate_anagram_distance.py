327907d1abb5f0016bada6a49fa9d36c770cbb17
haoran119 <34557994+haoran119@users.noreply.github.com>
2022-03-14 21:36:50 +1100

    Update calculate_anagram_distance.py

--- 面试总结之Python/calculate_anagram_distance.py
+++ 面试总结之Python/calculate_anagram_distance.py
@@ -3,27 +3,10 @@
 """
 https://www.geeksforgeeks.org/minimum-number-of-manipulations-required-to-make-two-strings-anagram-without-deletion-of-character/
 
-An anagram is a word that can be obtained by rearranging the letters of another word, using all the constituent letters
-exactly once. For example, the words "admirer" and "married" are anagrams of one another. However, the words "rather"
-and "harder" are not anagrams, since "rather" does not contain the letter 'a' and "harder" does not contain the letter
-'t'. Two identical words are anagrams, too. You may also notice that the anagram of any word must be of the same length
-as the word itself.
+Given two strings s1 and s2, we need to find the minimum number of manipulations required to make two strings anagram without 
+deleting any character. 
 
-Bob has written two words, A and B, consisting of N and M letters, respectively. He would like them to be anagrams of
-each other. Bob has decided to add some letters to these words to achieve his goal.
-
-For example, given the words "rather" and "harder", Bob can add the letter 'a' to the first word and the letter 't' to
-the second word. Having done this, the words are now anagrams.
-
-Bob would like to add as few letters as possible. In the example above, Bob added two letters, which is the minimum
-possible. Your task is to tell him the minimum number of letters that he needs to add to make the given words anagrams
-in this way.
-
-Write a function:
-
-int solution(char *A, char *B) ;
-
-that, given two non-empty strings, A and B, consisting of N and M letters respectively, returns the minimum number of
+Given two non-empty strings, A and B, consisting of N and M letters respectively, returns the minimum number of
 letters that Bob has to add to the words specified in A and B to make them anagrams.
 
 for example, give the words "rather" and "harder" your function should return 2 as explained above
